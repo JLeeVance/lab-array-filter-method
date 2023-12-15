e53dc29fc23dd6a79e560e479d5fb4f5cb6a45dc
JustinLeeVance <justinleevance94@gmail.com>
2023-12-15 12:30:13 -0500

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(array, string){
+    return array.filter(name => name.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch (array, string) {
+    return array.filter(name => name.startsWith(string))
+}
+function matchName (arrayOfDriverObjects, string) {
+    return arrayOfDriverObjects.filter(driver => driver.name == string)
+}
